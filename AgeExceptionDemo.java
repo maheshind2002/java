e436daddaab7b642a3b6d65444e127a73b367eae
mahesh2002 <maheshindalkar2002@gmail.com>
2025-05-14 10:14:12 +0530

    user_defined_exception

---
+++ AgeExceptionDemo.java
@@ -0,0 +1,35 @@
+class AgeException extends Exception{
+    AgeException(String msg){
+        super(msg);
+    }
+    AgeException(){
+
+    }
+}
+class Age{
+    void display(int age)throws AgeException{
+        if(age<18 || age>60){
+            throw new AgeException("Age should be in the 18 & 60");
+        }
+        else{
+            System.out.println("Enterd Age is Good...!");
+        }
+
+
+    }
+}
+public class AgeExceptionDemo {
+    public static void main(String[] args) {
+        System.out.println("Start Program");
+        Age a = new Age();
+        try {
+             a.display(16);
+        } catch (AgeException e) {
+           System.out.println(e.getMessage()); 
+        }
+       System.out.println("End Program");
+        
+        
+    }
+    
+}
