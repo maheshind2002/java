1fe48f86864eef25dc7da088e3a82d8602f1d33e
mahesh2002 <maheshindalkar2002@gmail.com>
2025-05-14 09:57:58 +0530

    OTP

--- Random.java
+++ Random.java
@@ -3,7 +3,7 @@ public class Random {
         System.out.println(Math.random()); // Generating random with decimal point 
         System.out.println((int)Math.random()); // Convert that decimal value into a one interger by typecasting using int
         System.out.println((int)(Math.random()* 10000)); // IT will Generate 4 digit random no everytime
-        System.out.println((int)(Math.random()* 1000000)); // IT will Generate 6 digit random no everytime
+        System.out.println((int)Math.random()* 1000000); // IT will Generate 6 digit random no everytime
      
 }
     
