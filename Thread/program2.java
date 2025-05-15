60a4bed22122f99a9afb786129045884658bc13a
mahesh2002 <maheshindalkar2002@gmail.com>
2025-05-15 19:32:13 +0530

    Multithreading(Runnable_Interface)

--- Thread/program2.java
+++ Thread/program2.java
@@ -1,5 +1,6 @@
 package Thread;
-
+// ---------------------------------*********** ----------------------------------*************--------------------------------
+// Creating the thread using runnable interface
 class ThreadDemo1 implements Runnable{
     public void run(){
         System.out.println("In Childs Thread");
@@ -10,8 +11,9 @@ class ThreadDemo1 implements Runnable{
 public class program2 {
     public static void main(String[] args) throws InterruptedException {
         ThreadDemo1 td1= new ThreadDemo1();
-        Thread t = new Thread(td1);
-        t.start();
+       // td1.start(); ----------------------- /// Here is a error due to the start methodd is only in the thread class
+        Thread t = new Thread(td1);// therfore here we have to create object of the thread class and pass td1 as arguement to it
+        t.start();                
         t.sleep(1000);
         System.out.println("In main Thread");
         
