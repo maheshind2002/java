765eedc613816258ae41cfcb12f7abc59a9b4b83
mahesh2002 <maheshindalkar2002@gmail.com>
2025-05-15 19:20:27 +0530

    Multithreading

--- Thread/program1.java
+++ Thread/program1.java
@@ -1,10 +1,10 @@
 package Thread;
 class ThreadDemo extends Thread{
     public void run(){
-       /* System.out.println("IN Child Thread");
+        System.out.println("IN Child Thread");
         System.out.println(Thread.currentThread()); // Display all properties of the thread [thread_name,priority,thread_grp]
         System.out.println(Thread.MIN_PRIORITY);
-        System.out.println(Thread.MAX_PRIORITY);*/
+        System.out.println(Thread.MAX_PRIORITY);
         try {
             Thread.sleep(2000);
             System.out.println("In child thread");
@@ -22,7 +22,7 @@ public class program1 {
     public static void main(String[] args) throws InterruptedException {
         ThreadDemo td = new ThreadDemo();
         td.start();
-       // td.setName("Thread_1");
+        td.setName("Thread_1");
         td.sleep(1000);
         System.out.println("In Main Thread");
     }
