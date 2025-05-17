a966b7bba9f1122956bf1c12f5701da372425159
mahesh2002 <maheshindalkar2002@gmail.com>
2025-05-17 23:22:20 +0530

    traversing_in_collection_foreach

---
+++ Collection/program3.java
@@ -0,0 +1,17 @@
+package Collection;
+import java.util.*;
+ class program3 {
+    public static void main(String[] args) {
+        List<Integer> l1 = new ArrayList<Integer>();
+        l1.add(12);
+        l1.add(20);
+        l1.add(30);
+        l1.add(40);
+        l1.add(50);
+
+        for(int x:l1){
+            System.out.println(x);
+        }
+    }
+    
+}
