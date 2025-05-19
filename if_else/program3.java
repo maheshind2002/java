106e454e3c08f34bc2d08d3a264028b8e33d8304
mahesh2002 <maheshindalkar2002@gmail.com>
2025-05-19 23:14:26 +0530

    problem_statement

--- if_else/program3.java
+++ if_else/program3.java
@@ -1,5 +1,5 @@
 package if_else;
-
+// check the no is greater than zero or not or the given no
 class Greater_Even{
 	public static void main(String[] args){
 
@@ -7,16 +7,16 @@ class Greater_Even{
 		if(num>0){
 			
 			
-	             	 if(num>=10 && num%2==0)
-			 {
-				System.out.println("Given no is even and also greater than 10");
-			}
-			else if	(num<10 && num%2==0){			
-				System.out.println(" given no is  a even number also less than 10");
-			}
-			else {
-				System.out.println("Number is odd number");
-			}
+		if(num>=10 && num%2==0)
+		 {
+			System.out.println("Given no is even and also greater than 10");
+		}
+		else if	(num<10 && num%2==0){			
+			System.out.println(" given no is  a even number also less than 10");
+		}
+		else {
+			System.out.println("Number is odd number");
+		}
 		}
 		else{
 			System.out.println("entered no not valid");
