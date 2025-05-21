58b364cfa97b8290bb4765f49215e0dbf9ae8d30
mahesh2002 <maheshindalkar2002@gmail.com>
2025-05-21 23:02:11 +0530

    problem_statement

---
+++ if_else2/program1.java
@@ -0,0 +1,15 @@
+package if_else2;
+// ckecking the number is between  1 to 1000 or not
+class Number{
+	public static void main(String[] args){
+		int num = 968;
+
+		if (num>=0 && num<=1000){
+			System.out.println(num +" given number is in 1 to 1000");
+		}
+		else{
+			System.out.println("number not in range");
+		}
+	}
+}
+
