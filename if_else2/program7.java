5544b5dfe8b30f8c1b72f8bb01f73d3fd08edf4f
mahesh2002 <maheshindalkar2002@gmail.com>
2025-05-22 23:20:37 +0530

    problem_statement

--- if_else2/program7.java
+++ if_else2/program7.java
@@ -1,5 +1,20 @@
 package if_else2;
-
-public class program7 {
-    
+// calculating profit,loss and discount
+class Pro7{
+	public static void main(String[] args){
+		int cost = 1023;
+		int selling = 650;
+		int profit = 0;
+		int loss = 0;
+		if(cost<=selling){
+			profit = selling - cost;
+			System.out.println(" profit of " + profit);
+		}
+		else{
+			loss = cost-selling;
+			System.out.println("loss of " + loss);
+		}
+	}
 }
+
+	
--- if_else2/program8.java
+++ if_else2/program8.java
@@ -1,5 +1,19 @@
 package if_else2;
-
-public class program8 {
-    
+// claculating the class based on the marks
+class Pro8{
+	public static void main(String[] args){
+		float percentage = 82.97f;
+		if(percentage>=75 && percentage<=100){
+			System.out.println("Passed : First class");
+		}
+		else if(percentage>=65 && percentage <=74){
+			System.out.println("Passed : Second class");
+		}
+		else if(percentage>=35 && percentage<=64){
+			System.out.println("Passed : Third class");
+		}
+		else{
+			System.out.println("Fail : Better luck next time");
+		}
+	}
 }
