05a3e40a5014b19d7b5d7dacd0904fa7317bde76
mahesh2002 <maheshindalkar2002@gmail.com>
2025-05-22 23:23:32 +0530

    problem_statement

--- if_else2/program9.java
+++ if_else2/program9.java
@@ -1,5 +1,15 @@
 package if_else2;
-
-public class program9 {
-    
+// based on values calculating the given triangle is pythagorus or not
+class Pro9{
+	public static void main (String[] args){
+		int a=5;
+		int b=3;
+		int c=8;
+		if(a*a+b*b==c*c){
+			System.out.println("it is a pythagorus theorem");
+		}
+		else{
+			System.out.println("it is not a pythagorus theorem");
+		}
+	}
 }
