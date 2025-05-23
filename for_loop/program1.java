e179424e924a794c2b233a0d40823ac5b28ef420
mahesh2002 <maheshindalkar2002@gmail.com>
2025-05-23 22:49:45 +0530

    problem_statement

--- for_loop/program1.java
+++ for_loop/program1.java
@@ -1,5 +1,11 @@
 package for_loop;
+// print 1 to 10 numbers
 
-public class program1 {
-    
+class Pro1{
+	public static void main(String[] args){
+		for(int i=1;i<=10;i++){
+			System.out.println(i);
+		}
+	}
 }
+
