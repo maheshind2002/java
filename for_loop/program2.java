37b6326d73bc0e81d4920f8ed6289473c4b6b3c7
mahesh2002 <maheshindalkar2002@gmail.com>
2025-05-23 22:50:50 +0530

    problem_statement

--- for_loop/program2.java
+++ for_loop/program2.java
@@ -1,5 +1,10 @@
 package for_loop;
-
-public class program2 {
-    
+// print 0 to 100
+class Pro2{
+	public static void main(String[] args){
+		for(int i = 0;i<=100;i++){
+			System.out.println(i);
+		}
+	}
 }
+
