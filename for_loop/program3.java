6e7b6b60b940ae10c7976bcd3f333e67e84a4151
mahesh2002 <maheshindalkar2002@gmail.com>
2025-05-23 22:53:20 +0530

    problem_statement

--- for_loop/program3.java
+++ for_loop/program3.java
@@ -1,5 +1,12 @@
 package for_loop;
-
-public class program3 {
-    
+// forloop with multiple print and increment operator
+class Pro3{
+	public static void main(String[] args){
+		int x = 100;
+		for(int i = 1;i<=10;i++){
+			System.out.print(x);
+            x++;
+			System.out.println(x);
+		}
+	}
 }
