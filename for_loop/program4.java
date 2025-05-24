d05bbb40866e25caccce2c17a5065f9e6b08a472
mahesh2002 <maheshindalkar2002@gmail.com>
2025-05-24 19:40:58 +0530

    problem_statement

--- for_loop/program4.java
+++ for_loop/program4.java
@@ -1,4 +1,11 @@
  package for_loop;
- public class program4 {
-    
+// print all even numbers
+ class Pro4{
+	public static void main(String[] args){
+		for(int i = 1;i<=100;i++){
+			if(i%2==0){
+				System.out.println(i);
+			}
+		}
+	}
 }
--- for_loop/program5.java
+++ for_loop/program5.java
@@ -1,5 +1,11 @@
 package for_loop;
-
-public class program5 {
-    
+// print all odd numbers
+class Pro5{
+	public static void main(String[] args){
+		for(int i = 1;i<=70;i++){
+			if(i%2==1){
+			System.out.println(i);
+			}
+		}
+	}
 }
