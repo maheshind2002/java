46d4d3e2c4e43ca06ddb73ada57a153dac1710d1
mahesh2002 <maheshindalkar2002@gmail.com>
2025-05-25 20:56:47 +0530

    problem_statement

--- for_loop2/program1.java
+++ for_loop2/program1.java
@@ -1,5 +1,13 @@
 package for_loop2;
-
-public class program1 {
-    
-}
+// use of while loop print no from 1 to 9
+class Pro3{
+	 public static void main(String[] args){
+		 int x=0;
+		 while(true){
+			 ++x;
+			 if(x%10==0)
+				 break;
+			 System.out.println(x);
+		 }
+	 }
+ }
