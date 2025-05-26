fdbfa5523d1f455c1f07faebfbbcd7a04f30d637
mahesh2002 <maheshindalkar2002@gmail.com>
2025-05-26 22:21:07 +0530

    problem_statement

--- while_loop/program7.java
+++ while_loop/program7.java
@@ -1,5 +1,15 @@
 package while_loop;
 
-public class program7 {
-    
-}
+// Write a Java program to print all uppercase English alphabets (A–Z) except the vowels (A, E, I, O, U).
+class Pro7{
+	public static void main(String[] args){
+		for(char ch = 65;ch<91;++ch){
+
+			if(ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U'){
+				continue;
+			}
+
+			       System.out.println(ch);
+		}
+	}
+}	
