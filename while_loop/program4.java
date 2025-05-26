c725386d093348c4a85c1268f86afd9786d07d1a
mahesh2002 <maheshindalkar2002@gmail.com>
2025-05-26 22:17:31 +0530

    problem_statement

--- while_loop/program4.java
+++ while_loop/program4.java
@@ -1,5 +1,17 @@
 package while_loop;
 
-public class program4 {
-    
+// Write a Java program that reverses a given integer number and displays the reversed number along with the original number
+class Pro4{
+	public static void main(String[] args){
+		int number = 301071;
+		int numVal=0;
+		int temp=number;
+		while(temp!=0){
+			numVal = numVal*10+(temp%10);
+			temp/=10;
+		}
+		System.out.println(temp);
+		System.out.println(numVal);
+		System.out.println(number);
+	}
 }
