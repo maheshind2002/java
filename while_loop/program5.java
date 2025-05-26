ecc24d1c6a866ba9a34014fb317155d62dd70981
mahesh2002 <maheshindalkar2002@gmail.com>
2025-05-26 22:18:52 +0530

    problem_statement

--- while_loop/program5.java
+++ while_loop/program5.java
@@ -1,5 +1,16 @@
 package while_loop;
 
-public class program5 {
-    
+// Write a Java program to print all positive integers less than 100 that are not divisible by 2, 3, 4, or 5.
+class Pro5{
+	public static void main(String[] args){
+		for(int i=1;i>0;i++){
+			if(i%2==0 || i%3==0 ||i%4==0 || i%5==0){
+				continue;
+			}
+			if(i==100){
+				break;
+			}
+			System.out.println(i+"");
+		}
+	}
 }
