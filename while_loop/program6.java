aae928c7ddcc958fad9ac44a386c28c8cd671a51
mahesh2002 <maheshindalkar2002@gmail.com>
2025-05-26 22:20:01 +0530

    problem_statement

--- while_loop/program6.java
+++ while_loop/program6.java
@@ -1,5 +1,12 @@
 package while_loop;
 
-public class program6 {
-    
+// Write a Java program that demonstrates the use of an infinite loop with a break condition
+class Pro6{
+	public static void main(String[] args){
+		int ch = 65;
+		while(1>0){
+			System.out.println(ch);
+			break;
+		}
+	}
 }
