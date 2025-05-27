938ef32f7c5739841217f3e4f37ae05ab12dd33c
mahesh2002 <maheshindalkar2002@gmail.com>
2025-05-27 22:58:28 +0530

    problem_statement

--- while_loop/program10.java
+++ while_loop/program10.java
@@ -1,5 +1,9 @@
 package while_loop;
 
+
+// Write a Java program that uses a while loop to iterate through numbers from 2 to 10 (inclusive). 
+//During each iteration, check if the current number is even. If it is even, print the dollar sign symbol $ followed by a newline.
+// If the number is odd, print a blank line.
 class Pro8{
 	public static void main(String[] args){
 		int i=1;
