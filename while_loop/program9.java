edc26544127ff082c75c40edafa62e25faac7262
mahesh2002 <maheshindalkar2002@gmail.com>
2025-05-27 23:01:07 +0530

    problem_Statement

--- while_loop/program9.java
+++ while_loop/program9.java
@@ -1,5 +1,17 @@
 package while_loop;
 
-public class program9 {
-    
+//Write a Java program that repeatedly decrements a number from 10 to 0, multiplying a temporary variable by the current
+// number in each step and printing the result.
+class Pro9{
+	public static void main(String[] args){
+		int num=10;
+        int temp=0;
+		while(num!=0){
+			num=num-1;
+			
+			temp*=num;
+			System.out.println(temp);
+		}
+		System.out.println(num);
+	}
 }
