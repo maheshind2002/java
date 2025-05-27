158b36feb9aba8eb23c7ccb00f8cd2d3af3fa1e7
mahesh2002 <maheshindalkar2002@gmail.com>
2025-05-27 23:02:55 +0530

    problem_statement

--- while_loop/program8.java
+++ while_loop/program8.java
@@ -1,5 +1,15 @@
 package while_loop;
 
-public class program8 {
-    
+//Write a Java program that counts the number of digits in a given integer.
+class Pro10{
+	public static void main(String[] args){
+		int num = 987654321;
+		int count = 0;
+		while(num!=0){
+			count++;
+			num/=10;
+		}
+		System.out.println(num);
+		System.out.println(count);
+	}
 }
