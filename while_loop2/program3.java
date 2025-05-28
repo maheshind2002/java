c0bec0960cf8cfcea3b72699fa42fb252f9ad3f7
mahesh2002 <maheshindalkar2002@gmail.com>
2025-05-28 22:07:05 +0530

    problem_statement

--- while_loop2/program3.java
+++ while_loop2/program3.java
@@ -1,5 +1,14 @@
 package while_loop2;
+// finding the numbers which are divisible by 4 and 7  in between 50 to 100
+class Pro3{
+	public static void main(String[] args){
+		int num = 100;
+		while(num>=50){
+			if(num%4==0 && num%7==0){
 
-public class program3 {
-    
+			System.out.println(num);
+			}
+			num--;
+		}
+	}
 }
