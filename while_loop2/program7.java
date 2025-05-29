62d47fa590c589053e7944c6291fd7bdd9f58a71
mahesh2002 <maheshindalkar2002@gmail.com>
2025-05-29 22:19:17 +0530

    problem_statement

--- while_loop2/program7.java
+++ while_loop2/program7.java
@@ -1,5 +1,12 @@
 package while_loop2;
 
-public class program7 {
-    
+// program to print the cube of numbers fron 1 to 10.
+class Pro7{
+	public static void main(String[] args){
+		int i = 1;
+		while(i<=10){
+			System.out.println(i*i*i);
+			i++;
+		}
+	}
 }
