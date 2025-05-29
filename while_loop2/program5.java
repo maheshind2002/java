c755c7169c97dfa917e2b0599601232462bbcbc9
mahesh2002 <maheshindalkar2002@gmail.com>
2025-05-29 22:16:10 +0530

    problem_statement

--- while_loop2/program5.java
+++ while_loop2/program5.java
@@ -1,4 +1,19 @@
 package while_loop2;
-public class program5 {
-    
+ 
+// Design and implement a Java program that simulates a countdown reminder for an upcoming deadline using a while loop
+class Pro5 {
+    public static void main(String[] args) {
+        int day = 7;
+        while (day >= 0) {
+            if (day > 0) {
+                System.out.println(day + " Days remaining...");
+
+            } else {
+                System.out.println(day + " hurry up its too late");
+            }
+            day--;
+
+        }
+
+    }
 }
