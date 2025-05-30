97588d34deac8426ec64800f8259eeeb627ff305
mahesh2002 <maheshindalkar2002@gmail.com>
2025-05-30 22:52:08 +0530

    problem_statement

--- while_loop2/program10.java
+++ while_loop2/program10.java
@@ -1,5 +1,14 @@
 package while_loop2;
 
-public class program10 {
-    
+
+class Pro10{
+	public static void main(String[] args){
+		int num = 100;
+		while(num>=24){
+			if(num%4==0 && num%5==0){
+				System.out.println(num);
+			}
+			num--;
+		}
+	}
 }
