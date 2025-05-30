850d031f429fd9afe4e6fc7c670f5008c4c1c74e
mahesh2002 <maheshindalkar2002@gmail.com>
2025-05-30 22:50:03 +0530

    problem_statement

--- while_loop2/program8.java
+++ while_loop2/program8.java
@@ -1,4 +1,17 @@
 package while_loop2;
-public class program8 {
-    
+
+class Pro8{
+	public static void main(String[] args){
+		int x = 90;
+		 int sum = 0;
+	
+		while(x>=11){
+
+			sum=sum+x;
+			x--;
+		
+	       }
+	        System.out.println(sum);
+	}
 }
+
