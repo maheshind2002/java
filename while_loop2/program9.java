8077b92105d76754b85173cf627d7c531c5ba107
mahesh2002 <maheshindalkar2002@gmail.com>
2025-05-30 22:51:02 +0530

    problem_statement

--- while_loop2/program9.java
+++ while_loop2/program9.java
@@ -1,4 +1,19 @@
 package while_loop2;
-public class program9 {
-    
+
+class Pro9{
+	public static void main(String[] args){
+		int add = 0;
+		int natural = 150;
+	       while(natural>=101){
+			if(natural%2==1){
+				add=add+natural;
+		
+			}
+			natural--;
+		
+		}
+	       System.out.println(add);
+	}
 }
+
+
