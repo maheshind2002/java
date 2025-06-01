6a35ce4fb8dac02cf32fbfe1d0a3b18117a16f04
mahesh2002 <maheshindalkar2002@gmail.com>
2025-06-01 20:33:33 +0530

    problem_statement

--- nested_for/program4.java
+++ nested_for/program4.java
@@ -1,5 +1,21 @@
 package nested_for;
 
-public class program4 {
-    
+// prdering to the number print that only alphabtes
+import java.util.*;
+class Pro_4{
+	public static void main(String[] args){
+		char cha='A';
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter row : ");
+		int row = sc.nextInt();
+		for(int i=1;i<=row;i++){
+			char ch=cha;
+			for(int j=i;j<=i;j++){
+				System.out.print(ch+" ");
+				ch++;
+			}
+			cha++;
+			System.out.println();
+		}
+	}
 }
