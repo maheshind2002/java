4f14c98da8731352d6b2d5f9ffb2679836dd2140
mahesh2002 <maheshindalkar2002@gmail.com>
2025-06-01 20:32:08 +0530

    problem_Statement

--- nested_for/program3.java
+++ nested_for/program3.java
@@ -1,4 +1,17 @@
 package nested_for;
-public class program3 {
-    
+import java.util.*;
+
+// print the number in reverse order
+class Pro_3{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter Rows : ");
+		int row = sc.nextInt();
+		for(int i=row;i>=1;i--){
+			for(int j=row;j>=i;j--){
+				System.out.print(j +" ");
+			}
+			System.out.println();
+		}
+	}
 }
