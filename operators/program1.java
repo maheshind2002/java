b492ce8f844b724acd598b10ad2e4bafc42285f1
mahesh2002 <maheshindalkar2002@gmail.com>
2025-06-02 21:53:09 +0530

    basic

---
+++ operators/program1.java
@@ -0,0 +1,19 @@
+package operators;
+
+class Pro1 {
+   Pro1() {
+   }
+
+   public static void main(String[] var0) {
+      int var1 = 0;
+      System.out.println(-var1);
+      System.out.println(var1);
+      ++var1;
+      System.out.println(var1);
+      --var1;
+      System.out.println(var1);
+      System.out.println(var1++);
+      System.out.println(var1--);
+   }
+}
+
