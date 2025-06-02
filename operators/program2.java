aa653975a92aa1b4390a4048eaeb6829f78e0bef
mahesh2002 <maheshindalkar2002@gmail.com>
2025-06-02 21:53:32 +0530

    basic

---
+++ operators/program2.java
@@ -0,0 +1,11 @@
+package operators;
+
+class Pro2 {
+   Pro2() {
+   }
+
+   public static void main(String[] var0) {
+      float var1 = 25.0F;
+      System.out.println("value of x is :" + var1);
+   }
+}
