c8da7e07891dc840628cdc9e9f99a6d5c560cb12
mahesh2002 <maheshindalkar2002@gmail.com>
2025-06-02 21:54:20 +0530

    basic

---
+++ operators/program5.java
@@ -0,0 +1,13 @@
+package operators;
+
+class Pro5 {
+   Pro5() {
+   }
+
+   public static void main(String[] var0) {
+      float var1 = 65.0F;
+      double var2 = (double)var1;
+      System.out.println(var2);
+   }
+}
+
