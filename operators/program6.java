6fdfe1050a7c3183a3566dbf7a55c66d78486181
mahesh2002 <maheshindalkar2002@gmail.com>
2025-06-03 21:27:29 +0530

    basic

---
+++ operators/program6.java
@@ -0,0 +1,13 @@
+package operators;
+
+class Pro6 {
+   Pro6() {
+   }
+
+   public static void main(String[] var0) {
+      byte var1 = 10;
+      System.out.println(~var1);
+      byte var2 = -11;
+      System.out.println(var2);
+   }
+}
