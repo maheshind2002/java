e8cdb0beb06094459bf5b87be02c142b0f1a0200
mahesh2002 <maheshindalkar2002@gmail.com>
2025-06-03 21:28:13 +0530

    basic

---
+++ operators/program9.java
@@ -0,0 +1,13 @@
+package operators;
+
+class Pro9{
+	public static void main(String[] args){
+		int x=0;
+		System.out.println(-x);  // -0 or 0
+		System.out.println(+x);  // +0 or 0
+		System.out.println(++x);  //1
+		System.out.println(--x);  // 0
+		System.out.println(x++);  // 0
+		System.out.println(x--);  // 1
+	}
+}
