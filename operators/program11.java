d750ad7f04a38cc1f2ee4f4ca55c100124ca6879
mahesh2002 <maheshindalkar2002@gmail.com>
2025-06-05 22:21:31 +0530

    basic

--- operators/program11.java
+++ operators/program11.java
@@ -1,1 +1,22 @@
 package operators;
+
+
+class Switchdemo2{
+	public static void main(String[] args){
+		int num = 50;
+		
+		switch(num){
+			case 25+25 :
+				System.out.println("25+25");
+				break;
+			case 30+25:
+				System.out.println("30+20");
+				break;
+			case 20+40:
+				System.out.println("20+30");
+				break;
+			default:
+				System.out.println("ghya shett");
+		}
+	}
+}
