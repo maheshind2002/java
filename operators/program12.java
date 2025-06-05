4b46fca0233b53679ce96a745178be47cf04047b
mahesh2002 <maheshindalkar2002@gmail.com>
2025-06-05 22:23:15 +0530

    basic

--- operators/program12.java
+++ operators/program12.java
@@ -1,5 +1,33 @@
 package operators;
 
-public class program12 {
-    
+
+
+class Switchdemo1{
+	public static void main(String [] args){
+		char ch  = 'C';
+
+		switch(ch){
+			case 'A':
+				System.out.println("Captial A");
+				break;
+			case 'a':
+				System.out.println("small a");
+				break;
+			case 'B':
+				System.out.println("Captial B");
+				break;
+			case 'C':
+				System.out.println("Captial C");
+				break;
+			case 'c':
+				System.out.println("small c");
+				break;
+			default :
+				System.out.println("Default case");
+
+				
+				
+		}
+	}
 }
+
