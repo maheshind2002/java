041ea3c4ca64159b09a760091f14f79aebdb27b9
mahesh2002 <maheshindalkar2002@gmail.com>
2025-06-05 22:24:53 +0530

    basic

--- operators/program13.java
+++ operators/program13.java
@@ -1,5 +1,36 @@
 package operators;
 
-public class program13 {
-    
+
+
+
+
+class Switchdemo{
+	public static void main(String[] args){
+		int x = 5;
+			
+		switch(x){
+			case 1:
+				System.out.println("one");
+				break;
+			
+			case 5 :
+				System.out.println("Five");
+				break;
+			
+			case 3 :
+				System.out.println("three");
+				break;
+			
+			case 4 : 
+				System.out.println("four");
+				break;
+			
+			case 2:
+				System.out.println("two");
+				break;
+
+			default:
+				System.out.println("Deafault");
+		}
+	}
 }
