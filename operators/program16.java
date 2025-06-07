660b8e936e37e0f29bfe06e761218a359bd300f9
mahesh2002 <maheshindalkar2002@gmail.com>
2025-06-07 23:12:30 +0530

    basic

--- operators/program16.java
+++ operators/program16.java
@@ -1,5 +1,14 @@
 package operators;
 
-public class program16 {
-    
+class Sum{
+	public static void main(String [] args){
+		int sum = 0;
+
+		for(int i = 1;i<=10;i++){
+			sum+=i;
+	
+		}
+		
+			System.out.println(sum);
+	}
 }
