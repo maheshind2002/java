5910d42f15af0e819ba4e2ab14370ba3fc798ff9
mahesh2002 <maheshindalkar2002@gmail.com>
2025-06-09 23:18:17 +0530

    basic

--- operators/program19.java
+++ operators/program19.java
@@ -1,21 +1,13 @@
 package operators;
 
-
-
-
-class Br1{
+class pro19{
 	public static void main(String[] args){
 		for(int i = 0;i<=50;i++){
 			if(i%5==0 && i%7==0){
 				break;
-			
 			}
-
 			System.out.println(i);
-			
 		}
-
-		
 
 	}
 }
