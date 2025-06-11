77d2958c2b00a50433faf2e9485f1da87cf0a052
mahesh2002 <maheshindalkar2002@gmail.com>
2025-06-11 22:50:53 +0530

    basic

--- operators/program15.java
+++ operators/program15.java
@@ -1,7 +1,15 @@
 package operators;
 
-public class program15 {
- 
-    
-    
+class Pro15{
+	public static void main(String[] args){
+		int i = 1;
+		int sum = 0;
+		while(i<=10){
+			sum = i*i*i;
+			System.out.println(sum);
+			i++;
+		}
+		System.out.println();
+	
+	}
 }
