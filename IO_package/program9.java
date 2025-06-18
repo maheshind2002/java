2a16b831d945442e003532c3232bc3358e525c7a
mahesh2002 <maheshindalkar2002@gmail.com>
2025-06-18 21:10:10 +0530

    Deserialization

---
+++ IO_package/program9.java
@@ -0,0 +1,37 @@
+package IO_package;
+import java.io.*;
+
+
+// Deserialization(Reading the data from the file is call deserialization)
+// Serializable interface should be implemented
+
+class Employee implements Serializable{
+    int empid ;
+    String empName;
+    int empSalary;
+
+    Employee(int empid,String empName,int empSalary){
+        this.empid = empid;
+        this.empName = empName;
+        this.empSalary = empSalary;
+    }
+    Employee(){
+        // deafault constructor
+    }
+    public String toString(){
+        return "empid :" + empid + "\n"+ "empName :"+empName +"\n"+"empSalary :"+ empSalary;
+    }
+}
+public class program9 {
+     public static void main(String[] args) throws IOException,ClassNotFoundException
+     {
+        FileInputStream fis = new FileInputStream("emp.txt");
+        ObjectInputStream ois = new ObjectInputStream(fis);
+        Employee emp1 = (Employee) ois.readObject();
+        System.out.println(emp1);
+        ois.close();
+        fis.close();
+        
+        
+     }
+}
