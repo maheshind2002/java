309f6c046c267a6be06cc27110d7434fced96408
mahesh2002 <maheshindalkar2002@gmail.com>
2025-06-19 19:41:14 +0530

    Duplicate_from _sorted array

---
+++ Duplicate.java
@@ -0,0 +1,22 @@
+
+// removeing duplicate from sorted array in place without using extra space
+
+public class Duplicate {
+        public static void main(String[] args) {
+        int [] arr = new int[]{21,35,35,47,47,56,68};
+        int j = 0;
+
+        for(int i =1;i<arr.length;i++){
+            if(arr[i] != arr[j]){
+                j++;
+                int temp = arr[j];
+                arr[j]=arr[i];
+                arr[i]= temp;
+            }
+        }
+        for(int i =0;i<=j;i++){
+            System.out.println(arr[i]+ " ");
+        }
+        
+    }
+}
