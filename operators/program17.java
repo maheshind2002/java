d039c9e9d56449403394bc7e08718829ae298c2d
mahesh2002 <maheshindalkar2002@gmail.com>
2025-06-27 22:31:31 +0530

    basic

--- operators/program17.java
+++ operators/program17.java
@@ -2,10 +2,10 @@ package operators;
 
 class Product{
  public static void main(String [] args){
-         int product = 1;
-                for(int i = 1;i<=10;i++){
-                        product*=i;
-                }
-             System.out.println(product);   
+        int product = 1;
+         for(int i = 1;i<=10;i++){
+                product*=i;
+         }
+        System.out.println(product);   
         }
 }
